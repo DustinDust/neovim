484e5de6d5aa916630d4fa5ad23e7c414f84a469
NamDang25 <79992369+DustinDust@users.noreply.github.com>
2024-05-09 13:38:01 +0000

    initial commit

---
+++ lua/shuy/lazy/lazygit.lua
@@ -0,0 +1,17 @@
+return {
+	"kdheepak/lazygit.nvim",
+	-- optional for floating window border decoration
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	cmd          = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	keys         = {
+		{ "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+	}
+}
