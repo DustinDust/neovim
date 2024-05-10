d49a7ad8a7c4c5470f31e49197a71eb713468f81
NamDang25 <79992369+DustinDust@users.noreply.github.com>
2024-05-10 18:52:11 +0000

    add telescope keybind for buffers & fuzzy find

--- lua/shuy/lazy/telescope.lua
+++ lua/shuy/lazy/telescope.lua
@@ -1,24 +1,27 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		require('telescope').setup({})
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        require('telescope').setup({})
 
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>pws', function()
-            		local word = vim.fn.expand("<cword>")
-            		builtin.grep_string({ search = word })
-        	end)
-	        vim.keymap.set('n', '<leader>pWs', function()
-       	    		local word = vim.fn.expand("<cWORD>")
-             		builtin.grep_string({ search = word })
-        	end)
-        	vim.keymap.set('n', '<leader>ps', function()
-            		builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        	end)
-       		vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-	end
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pws', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>pWs', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        vim.keymap.set('n', '<leader>pls', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>be', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>bs', builtin.current_buffer_fuzzy_find, {})
+    end
 }
-
