3eb9af9cb1a3600443af28ef48a7218794deae14
namdp-pixta <nam.dangphuong@pixta.co.jp>
2024-05-16 09:28:30 +0700

    added more theme & update keybind

--- lua/shuy/lazy/colors.lua
+++ lua/shuy/lazy/colors.lua
@@ -1,27 +1,73 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+    -- default colorscheme "rose-pine"
+    color = color or "rose-pine"
 
---	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+    -- rose-pine config
+    require('rose-pine').setup({
+        disable_background = false,
+        styles = {
+            italic = false,
+        },
+    })
+
+    -- gruvbox-material config
+    vim.g.gruvbox_material_foreground = "mix"
+
+    -- vscode config
+    local c = require('vscode.colors').get_colors()
+    require('vscode').setup({
+        -- Alternatively set style in setup
+        -- style = 'light'
+
+        -- Enable transparent background
+        transparent = false,
+
+        -- Enable italic comment
+        italic_comments = true,
+
+        -- Underline `@markup.link.*` variants
+        underline_links = true,
+
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
+
+        -- Override colors (see ./lua/vscode/colors.lua)
+        -- color_overrides = {
+        --   vscLineNumber = '#FFFFFF',
+        -- },
+
+        -- Override highlight groups (see ./lua/vscode/theme.lua)
+        group_overrides = {
+            -- this supports the same val table as vim.api.nvim_set_hl
+            -- use colors from this colorscheme by requiring vscode.colors!
+            Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+        }
+    })
+
+    vim.cmd.colorscheme(color)
+
+    -- other config
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-	{
+    {
         "rose-pine/neovim",
         name = "rose-pine",
         config = function()
-            require('rose-pine').setup({
-                disable_background = false,
-                styles = {
-                    italic = false,
-                },
-            })
-
-            vim.cmd("colorscheme rose-pine")
-
-            ColorMyPencils()
         end
-	},
+    },
+    {
+        "sainnhe/gruvbox-material",
+        config = function()
+        end
+    },
+    {
+        'Mofiqul/vscode.nvim',
+        config = function()
+            ColorMyPencils("vscode")
+        end
+    }
 }
