a3800a83089fa574daef662eda5754ff06bf862f
namdp-pixta <nam.dangphuong@pixta.co.jp>
2024-07-05 15:16:03 +0700

    update remap for saving

--- lua/shuy/remap.lua
+++ lua/shuy/remap.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
 vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('n', '<leader>q', '<esc>')
+vim.keymap.set('n', '<C-s>', vim.cmd.write)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
