da7ab0f492fe68b4bdf6adb41174d9f691985a21
Nam Dang <dustidusk@gmail.com>
2024-10-16 21:50:29 +0700

    udpate deps

--- lua/shuy/lazy/colors.lua
+++ lua/shuy/lazy/colors.lua
@@ -27,9 +27,6 @@ function ColorMyPencils(color)
             palette = {},
             theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
         },
-        overrides = function(colors) -- add/modify highlights
-            return {}
-        end,
         theme = "wave",    -- Load "wave" theme when 'background' option is not set
         background = {     -- map the value of 'background' option to a theme
             dark = "wave", -- try "dragon" !
@@ -73,23 +70,22 @@ function ColorMyPencils(color)
 
     require("catppuccin").setup({
         flavour = "mocha", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
+        background = {     -- :h background
             light = "latte",
             dark = "mocha",
         },
         transparent_background = false, -- disables setting the background color.
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = false,        -- sets terminal colors (e.g. `g:terminal_color_0`)
+        show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+        term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
-            enabled = true,        -- dims the background color of inactive window
+            enabled = true,             -- dims the background color of inactive window
             shade = "dark",
-            percentage = 0.15,      -- percentage of the shade to apply to the inactive window
+            percentage = 0.15,          -- percentage of the shade to apply to the inactive window
         },
-        no_italic = false,          -- Force no italic
-        no_bold = false,            -- Force no bold
-        no_underline = false,       -- Force no underline
-        styles = {                  -- Handles the styles of general hi groups (see `:h highlight-args`):
-            comments = { "italic" }, -- Change the style of comments
+        no_italic = false,              -- Force no italic
+        no_bold = false,                -- Force no bold
+        no_underline = false,           -- Force no underline
+        styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`): comments = { "italic" }, -- Change the style of comments
             conditionals = { "italic" },
             loops = {},
             functions = {},
