7a85d37ee644489beb315eac02a4df8eed5950b8
namdp-pixta <nam.dangphuong@pixta.co.jp>
2025-07-24 08:45:03 +0700

    update noice config

--- lua/shuy/lazy/noice.lua
+++ lua/shuy/lazy/noice.lua
@@ -30,6 +30,16 @@ return {
                 inc_rename = false,           -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = true,
             },
+            messages = {
+                -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+                -- This is a current Neovim limitation.
+                enabled = true,              -- enables the Noice messages UI
+                view = "mini",            -- default view for messages
+                view_error = "mini",      -- view for errors
+                view_warn = "mini",       -- view for warnings
+                view_history = "messages",   -- view for :messages
+                view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+            },
         })
         require("telescope").load_extension("noice")
     end
